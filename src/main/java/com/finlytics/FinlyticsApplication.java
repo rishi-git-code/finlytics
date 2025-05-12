53b317ba1c9ca6a26ecaedc156d664ac32ccf2d8
RishikeshKumar <rishikeshk035@gmail.com>
2025-05-12 13:57:35 +0530

    Folder added

---
+++ src/main/java/com/finlytics/FinlyticsApplication.java
@@ -0,0 +1,13 @@
+package com.finlytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinlyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinlyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finlytics/FinlyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finlytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinlyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
