8e3ffaa8683cc867930b4fc076b967c387ffab8d
RishikeshKumar <rishikeshk035@gmail.com>
2025-06-22 03:08:43 +0530

    code changes for user

--- src/main/java/com/finlytics/utils/JwtUtil.java
+++ src/main/java/com/finlytics/utils/JwtUtil.java
@@ -4,19 +4,24 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.security.Key;
 
 public class JwtUtil {
 
-    private static final Key key =Keys.secretKeyFor(SignatureAlgorithm.HS256);
+//    private static final Key key =Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private static final String SECRET = "your-256-bit-secret-key-should-be-shared-between-services"; // store securely
+    private static final Key key = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
     private static final long EXPIRATION_TIME = 86400000; // 1 day
+    private static final SignatureAlgorithm ALGORITHM = SignatureAlgorithm.HS384;
+
 
     public static String generateToken(String username){
         return Jwts.builder()
                 .setSubject(username)
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(key)
+                .signWith(key,ALGORITHM)
                 .compact();
     }
 
