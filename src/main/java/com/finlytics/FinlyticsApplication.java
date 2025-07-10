3943994646b76f8827b8f3c2c8a670f532d01b0d
RishikeshKumar <rishikeshk035@gmail.com>
2025-07-11 01:12:22 +0530

    code changes for user

--- src/main/java/com/finlytics/FinlyticsApplication.java
+++ src/main/java/com/finlytics/FinlyticsApplication.java
@@ -1,5 +1,6 @@
 package com.finlytics;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FinlyticsApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
 		SpringApplication.run(FinlyticsApplication.class, args);
 	}
 
